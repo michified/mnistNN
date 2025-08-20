430e0807133c0ce96cbb77c2ff228788900f1134
Michael <michaelyi.1239@gmail.com>
2025-08-20 10:59:30 +0800

    reverted scale factor

--- scripts/demo.py
+++ scripts/demo.py
@@ -159,7 +159,7 @@ def scale_coords():
     boottom_right_corner = (max(x for x, y in draw_coords), max(y for x, y in draw_coords))
     center = ((top_left_corner[0] + boottom_right_corner[0]) // 2, (top_left_corner[1] + boottom_right_corner[1]) // 2)
     scale_factor = min(WIDTH // (boottom_right_corner[0] - top_left_corner[0] + 1), 
-                       HEIGHT // (boottom_right_corner[1] - top_left_corner[1] + 1)) / 1.5
+                       HEIGHT // (boottom_right_corner[1] - top_left_corner[1] + 1))
     for x, y in draw_coords:
         scaled_x = int((x - center[0]) * scale_factor + WIDTH // 2)
         scaled_y = int((y - center[1]) * scale_factor + HEIGHT // 2)
